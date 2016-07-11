ed7a10df8887273a8ec03941b2d02d1f2bf04c94
Naveed Fida <naveedfida01@gmail.com>
2016-07-11 07:19:36 -0700

    Final Commit

--- utilities.js
+++ utilities.js
@@ -1,25 +1,202 @@
-function _(element) {
-  u = {
-    first: function() {
-      return element[0];
-    },
+(function() {
+  function _(element) {
+    u = {
+      first: function() {
+        return element[0];
+      },
 
-    last: function() {
-      return element[element.length -1]; 
-    },
+      last: function() {
+        return element[element.length -1]; 
+      },
 
-    without: function() {
-      var args = Array.from(arguments);
-      var newArray = [];
-      for ( var i = 0; i < element.length; i++) {
-        if (!args.includes(element[i])) newArray.push(element[i]);
-      }
-      return newArray;
-    },
+      without: function() {
+        var args = Array.from(arguments);
+        var newArray = [];
 
-    
+        element.forEach(function(elem) {
+          if (!args.includes(elem)) newArray.push(elem);
+        });
+         return newArray;
+      },
 
+      lastIndexOf: function(elem) {
+        return element.lastIndexOf(elem);
+      },
+
+      sample: function(qty) {
+        var sampled = [];
+        var elem_copy = element.slice();
+
+        get = function() {
+          var idx = Math.floor(Math.random() * elem_copy.length),
+              el = elem_copy[idx];
+          elem_copy.splice(idx, 1);
+          return el;
+        }
+        if (!qty) { return get(); }
+        while (qty) {
+          sampled.push(get());
+          qty--;
+        }
+        return sampled;
+      },
+
+      findWhere: function(query) {
+        var query_keys = Object.keys(query);
+        for (var i = 0; i < element.length; i++) {
+          var obj = element[i];
+          var all_match = query_keys.every(function(key) {
+            return query[key] === obj[key];
+          });
+          if (all_match) { return obj; }
+        }
+      },
+
+      where: function(query) {
+        var query_keys = Object.keys(query);
+        var return_arr = [];
+        for (var i = 0; i < element.length; i++) {
+          var obj = element[i];
+          var all_match = query_keys.every(function(key) {
+            return query[key] === obj[key];
+          });
+          if (all_match) { return_arr.push(obj); }
+        }
+        return return_arr;
+      },
+
+      pluck: function(prop) {
+        var values = [];
+        element.forEach(function(obj) {
+          if(obj[prop]) { values.push(obj[prop]); }
+        });
+        return values;
+      },
+
+      keys: function() {
+        return Object.keys(element);
+      },
+
+      values: function() {
+        var arr_values = [];
+        for (var prop in element) {
+          arr_values.push(element[prop]);
+        }
+        return arr_values;
+      },
+
+      pick: function() {
+        props = Array.from(arguments);
+        newObj = {};
+
+        props.forEach(function(prop) {
+          if (element[prop]) { newObj[prop] = element[prop] };
+        });
+        return newObj;
+      },
+
+      omit: function() {
+        props = Array.from(arguments);
+        newObj = {};
+
+        for (var prop in element) {
+          if(props.indexOf(prop) == -1) { newObj[prop] = element[prop]; }
+        }
+        return newObj;
+      },
+
+      has: function(prop) {
+        return {}.hasOwnProperty.call(element, prop);
+        // return Object.keys(element).includes(prop);
+      },
+
+      // isElement: function() {
+      //   return element instanceof HTMLElement;
+      // },
+
+      // isArray: function() {
+      //   return element instanceof Array;
+      // },
+
+      // isObject: function() {
+      //   return element instanceof Object;
+      // },
+
+      // isFunction: function() {
+      //   return element instanceof Function;
+      // },
+
+      // isBoolean: function() {
+      //   return (typeof element) === "boolean" || element instanceof Boolean;
+      // },
+
+      // isString: function() {
+      //   return (typeof element) == "string" || element instanceof String;
+      // },
+
+      // isNumber: function() {
+      //   return (typeof element) == 'number' || element instanceof Number;
+      // }
+
+    };
+
+    (["isElement", "isArray", "isObject", "isFunction", "isBoolean", "isString", "isNumber"]).forEach(function(method) {
+      u[method] = function() { return _[method].call(u, element); };
+    });
+
+    return u;
+  };
+  _.range = function(low, high) {
+    var range = [];
+    if(high === undefined) {
+      high = low;
+      low = 0;
+    }
+    for (var i = low; i < high; i++) {
+      range.push(i);
+    }
+    return range;
   };
 
-  return u;
-}
+  _.extend = function() {
+    args = Array.from(arguments);
+    old_obj = args.shift();
+
+    args.forEach(function(obj) {
+      for (prop in obj) {
+        old_obj[prop] = obj[prop];
+      }
+    });
+    return old_obj;
+  };
+
+  _.isElement = function(thing) {
+    return thing instanceof HTMLElement;
+  };
+
+  _.isArray = Array.isArray || function(thing) {
+    return thing instanceof Array;
+  };
+
+  _.isObject = function(thing) {
+    return thing instanceof Object;
+  };
+
+  _.isFunction = function(thing) {
+    return thing instanceof Function;
+  };
+
+  _.isBoolean = function(thing) {
+    return (typeof thing) === "boolean" || thing instanceof Boolean;
+  };
+
+  _.isString = function(thing) {
+    return (typeof thing) == "string" || thing instanceof String;
+  };
+
+  _.isNumber = function(thing) {
+    return (typeof thing) == 'number' || thing instanceof Number;
+  };
+
+  window._ = _;
+})();
--- utility_tests.js
+++ utility_tests.js
@@ -59,230 +59,273 @@ test("without removes any number of arguments", function() {
   return a.indexOf(6) === -1 && a.indexOf(4) === -1;
 });
 
-// // _.range
-// test("range is defined", function() {
-//   return typeof _.range === "function";
-// });
-// test("range returns an array of values starting at 0 when one argument supplied", function() {
-//   return _.range(10)[0] === 0 && _.range(10).length === 10;
-// });
-// test("range returns an array of values starting with the first value when two arguments supplied", function() {
-//   return _.range(1, 10)[0] === 1 && _.range(1, 10).length === 9;
-// });
+// _.range
+test("range is defined", function() {
+  return typeof _.range === "function";
+});
+test("range returns an array of values starting at 0 when one argument supplied", function() {
+  return _.range(10)[0] === 0 && _.range(10).length === 10;
+});
+test("range returns an array of values starting with the first value when two arguments supplied", function() {
+  return _.range(1, 10)[0] === 1 && _.range(1, 10).length === 9;
+});
 
-// // _.lastIndexOf
-// test("lastIndexOf is defined", function() {
-//   return typeof _().lastIndexOf === "function";
-// });
-// test("lastIndexOf returns last index of supplied value", function() {
-//   return _([1, 1, 1]).lastIndexOf(1) === 2 && _([1, 2, 3]).lastIndexOf(2) === 1;
-// });
+// _.lastIndexOf
+test("lastIndexOf is defined", function() {
+  return typeof _().lastIndexOf === "function";
+});
+test("lastIndexOf returns last index of supplied value", function() {
+  return _([1, 1, 1]).lastIndexOf(1) === 2 && _([1, 2, 3]).lastIndexOf(2) === 1;
+});
 
-// // _.sample
-// test("sample is defined", function() {
-//   return typeof _().sample === "function";
-// });
-// test("sample returns a single element when no argument supplied", function() {
-//   return _([1]).sample() === 1;
-// });
-// test("sample returns multiple, non-repetitive elements when a numeric argument supplied", function() {
-//   return _([1, 2, 3]).sample(3).length === 3;
-// });
+// _.sample
+test("sample is defined", function() {
+  return typeof _().sample === "function";
+});
+test("sample returns a single element when no argument supplied", function() {
+  return _([1]).sample() === 1;
+});
+test("sample returns multiple, non-repetitive elements when a numeric argument supplied", function() {
+  return _([1, 2, 3]).sample(3).length === 3;
+});
 
-// // _.findWhere
-// test("findWhere is defined", function() {
-//   return typeof _().findWhere === "function";
-// });
-// (function() {
-//   var dict = [{ foo: "bar", idx: 0 }, { foo: "baz", idx: 1 }, { foo: "bar", idx: 2 }];
+// _.findWhere
+test("findWhere is defined", function() {
+  return typeof _().findWhere === "function";
+});
+(function() {
+  var dict = [{ foo: "bar", idx: 0 }, { foo: "baz", idx: 1 }, { foo: "bar", idx: 2 }];
 
-//   test("findWhere returns the first object with matched properties", function() {
-//     return _(dict).findWhere({ foo: "bar" }).idx === 0;
-//   });
-// })();
-// (function() {
-//   var dict = [{ foo: "bar", quux: "q", idx: 0 }, { foo: "baz", quux: "z", idx: 1 }, { foo: "bar", quux: "z", idx: 2 }];
+  test("findWhere returns the first object with matched properties", function() {
+    return _(dict).findWhere({ foo: "bar" }).idx === 0;
+  });
+})();
+(function() {
+  var dict = [{ foo: "bar", quux: "q", idx: 0 }, { foo: "baz", quux: "z", idx: 1 }, { foo: "bar", quux: "z", idx: 2 }];
 
-//   test("findWhere returns the first object with multiple matched properties", function() {
-//     return _(dict).findWhere({ foo: "bar", quux: "z" }).idx === 2;
-//   });
-// })();
-// (function() {
-//   var dict = [{ foo: "bar", idx: 0 }, { foo: "baz", idx: 1 }, { foo: "bar", idx: 2 }];
+  test("findWhere returns the first object with multiple matched properties", function() {
+    return _(dict).findWhere({ foo: "bar", quux: "z" }).idx === 2;
+  });
+})();
+(function() {
+  var dict = [{ foo: "bar", idx: 0 }, { foo: "baz", idx: 1 }, { foo: "bar", idx: 2 }];
 
-//   test("findWhere returns undefined with no matched properties", function() {
-//     return _(dict).findWhere({ foo: "quux" }) === undefined;
-//   });
-// })();
+  test("findWhere returns undefined with no matched properties", function() {
+    return _(dict).findWhere({ foo: "quux" }) === undefined;
+  });
+})();
 
-// // _.where
-// test("where is defined", function() {
-//   return typeof _().where === "function";
-// });
+// _.where
+test("where is defined", function() {
+  return typeof _().where === "function";
+});
 
-// (function() {
-//   var dict = [{ foo: "bar", idx: 0 }, { foo: "baz", idx: 1 }, { foo: "bar", idx: 2 }];
+(function() {
+  var dict = [{ foo: "bar", idx: 0 }, { foo: "baz", idx: 1 }, { foo: "bar", idx: 2 }];
 
-//   test("where returns an array with one matched object", function() {
-//     return _(dict).where({ idx: 0 }).length === 1;
-//   });
-//   test("where returns an array with two matched objects", function() {
-//     return _(dict).where({ foo: "bar" }).length === 2;
-//   });
-// })();
+  test("where returns an array with one matched object", function() {
+    return _(dict).where({ idx: 0 }).length === 1;
+  });
+  test("where returns an array with two matched objects", function() {
+    return _(dict).where({ foo: "bar" }).length === 2;
+  });
+})();
 
-// // _.pluck
-// test("pluck is defined", function() {
-//   return typeof _().pluck === "function";
-// });
+// _.pluck
+test("pluck is defined", function() {
+  return typeof _().pluck === "function";
+});
 
-// test("pluck returns array of two values", function() {
-//   var coll = [{ foo: "bar" }, { foo: "baz" }],
-//       pluck = _(coll).pluck("foo");
+test("pluck returns array of two values", function() {
+  var coll = [{ foo: "bar" }, { foo: "baz" }],
+      pluck = _(coll).pluck("foo");
 
-//   return pluck.length === 2;
-// });
-// test("pluck returns both values", function() {
-//   var coll = [{ foo: "bar" }, { foo: "baz" }],
-//       pluck = _(coll).pluck("foo");
+  return pluck.length === 2;
+});
+test("pluck returns both values", function() {
+  var coll = [{ foo: "bar" }, { foo: "baz" }],
+      pluck = _(coll).pluck("foo");
 
-//   return pluck[0] === "bar" && pluck[1] === "baz";
-// });
+  return pluck[0] === "bar" && pluck[1] === "baz";
+});
 
-// // _.keys
-// test("keys is defined", function() {
-//   return typeof _().keys === "function";
-// });
+// _.keys
+test("keys is defined", function() {
+  return typeof _().keys === "function";
+});
 
-// test("keys returns an array of keys from the object", function() {
-//   var keys = _({ foo: "bar", baz: "quuz" }).keys();
-//   return keys.length === 2;
-// });
-// test("keys returns all keys that are own properties of the object", function() {
-//   var keys = _({ foo: "bar", baz: "quuz" }).keys();
-//   return keys.indexOf("foo") !== -1 && keys.indexOf("baz") !== -1;
-// });
-// test("keys does not return inherited object properties", function() {
-//   var keys = _({ foo: "bar", baz: "quuz" }).keys();
-//   return keys.indexOf("toString") === -1;
-// });
+test("keys returns an array of keys from the object", function() {
+  var keys = _({ foo: "bar", baz: "quuz" }).keys();
+  return keys.length === 2;
+});
+test("keys returns all keys that are own properties of the object", function() {
+  var keys = _({ foo: "bar", baz: "quuz" }).keys();
+  return keys.indexOf("foo") !== -1 && keys.indexOf("baz") !== -1;
+});
+test("keys does not return inherited object properties", function() {
+  var keys = _({ foo: "bar", baz: "quuz" }).keys();
+  return keys.indexOf("toString") === -1;
+});
 
-// // _.values
-// test("values is defined", function() {
-//   return typeof _().values === "function";
-// });
+// _.values
+test("values is defined", function() {
+  return typeof _().values === "function";
+});
 
-// test("values returns an array of values from the object", function() {
-//   var values = _({ foo: "bar", baz: "quuz" }).values();
-//   return values.length === 2;
-// });
-// test("values returns all values that are own properties of the object", function() {
-//   var values = _({ foo: "bar", baz: "quuz" }).values();
-//   return values.indexOf("bar") !== -1 && values.indexOf("quuz") !== -1;
-// });
+test("values returns an array of values from the object", function() {
+  var values = _({ foo: "bar", baz: "quuz" }).values();
+  return values.length === 2;
+});
 
-// // _.extend
-// test("extend is defined", function() {
-//   return typeof _.extend === "function";
-// });
+test("values returns all values that are own properties of the object", function() {
+  var values = _({ foo: "bar", baz: "quuz" }).values();
+  return values.indexOf("bar") !== -1 && values.indexOf("quuz") !== -1;
+});
 
-// test("extend returns an extended object using new object's values", function() {
-//   var new_obj = { bar: "baz" },
-//       old_obj = { foo: "bar" },
-//       ext_obj = _.extend(old_obj, new_obj);
-//       crazy_object = _.extend({ foo: "quuz" }, new_obj, old_obj);
-//   return ext_obj.foo === "bar" && ext_obj.bar === "baz";
-// });
-// test("extend modifies the first object passed in rather than creating a new object", function() {
-//   var new_obj = { bar: "baz" },
-//       old_obj = { foo: "bar" },
-//       ext_obj = _.extend(old_obj, new_obj);
-//       crazy_object = _.extend({ foo: "quuz" }, new_obj, old_obj);
-//   return new_obj === _.extend(new_obj, {});
-// });
-// test("extend works with any number of objects", function() {
-//   var new_obj = { bar: "baz" },
-//       old_obj = { foo: "bar" },
-//       ext_obj = _.extend(old_obj, new_obj);
-//       crazy_object = _.extend({ foo: "quuz" }, new_obj, old_obj);
-//   return crazy_object.foo === "bar";
-// });
+// _.extend
+test("extend is defined", function() {
+  return typeof _.extend === "function";
+});
 
-// // _.pick
-// test("pick is defined", function() {
-//   return typeof _().pick === "function";
-// });
+test("extend returns an extended object using new object's values", function() {
+  var new_obj = { bar: "baz" },
+      old_obj = { foo: "bar" },
+      ext_obj = _.extend(old_obj, new_obj);
+      crazy_object = _.extend({ foo: "quuz" }, new_obj, old_obj);
+  return ext_obj.foo === "bar" && ext_obj.bar === "baz";
+});
+test("extend modifies the first object passed in rather than creating a new object", function() {
+  var new_obj = { bar: "baz" },
+      old_obj = { foo: "bar" },
+      ext_obj = _.extend(old_obj, new_obj);
+      crazy_object = _.extend({ foo: "quuz" }, new_obj, old_obj);
+  return new_obj === _.extend(new_obj, {});
+});
+test("extend works with any number of objects", function() {
+  var new_obj = { bar: "baz" },
+      old_obj = { foo: "bar" },
+      ext_obj = _.extend(old_obj, new_obj);
+      crazy_object = _.extend({ foo: "quuz" }, new_obj, old_obj);
+  return crazy_object.foo === "bar";
+});
 
-// test("pick returns a new object with the passed in properties", function() {
-//   var old_obj = { foo: "bar" },
-//       new_obj = _(old_obj).pick("foo");
+// _.pick
+test("pick is defined", function() {
+  return typeof _().pick === "function";
+});
 
-//   return new_obj.foo === old_obj.foo && new_obj !== old_obj;
-// });
-// test("pick ignores any properties passed in that do not exist on the source object", function() {
-//   var old_obj = { foo: "bar" },
-//       new_obj = _(old_obj).pick("foo");
+test("pick returns a new object with the passed in properties", function() {
+  var old_obj = { foo: "bar" },
+      new_obj = _(old_obj).pick("foo");
 
-//   return _(new_obj).pick("foo", "bar").bar === undefined;
-// });
+  return new_obj.foo === old_obj.foo && new_obj !== old_obj;
+});
+test("pick ignores any properties passed in that do not exist on the source object", function() {
+  var old_obj = { foo: "bar" },
+      new_obj = _(old_obj).pick("foo");
 
-// // _.omit
-// test("omit is defined", function() {
-//   return typeof _().omit === "function";
-// });
+  return _(new_obj).pick("foo", "bar").bar === undefined;
+});
 
-// test("omit returns a new object with any passed in properties omitted", function() {
-//   var old_obj = { foo: "bar" },
-//       new_obj = _(old_obj).omit("foo");
+test("pick can return an object with more than one properties", function() {
+  var old_obj = {foo: 1, bar: 2, baz: 3, bac: 4},
+      new_obj = _(old_obj).pick("foo", "baz");
+      console.log(new_obj);
+  return new_obj.foo === 1 && new_obj.baz === 3 && new_obj.bac === undefined;
+});
 
-//   return new_obj.foo === undefined;
-// });
+// _.omit
+test("omit is defined", function() {
+  return typeof _().omit === "function";
+});
 
-// // _.has
-// test("has is defined", function() {
-//   return typeof _().has === "function";
-// });
+test("omit returns a new object with any passed in properties omitted", function() {
+  var old_obj = { foo: "bar", baz: "okay" },
+      new_obj = _(old_obj).omit("foo");
 
-// test("has returns true when property exists", function() {
-//   var o = { foo: "bar" };
+  return new_obj.foo === undefined && new_obj.baz === "okay";
+});
 
-//   return _(o).has("foo");
-// });
-// test("has returns false when property does not exist", function() {
-//   var o = { foo: "bar" };
+// _.has
+test("has is defined", function() {
+  return typeof _().has === "function";
+});
 
-//   return !_(o).has("bar");
-// });
-// test("has returns true when hasOwnProperty is defined", function() {
-//   var o = { foo: "bar" };
-//   o.hasOwnProperty = function() { };
+test("has returns true when property exists", function() {
+  var o = { foo: "bar" };
 
-//   return _(o).has("hasOwnProperty");
-// });
+  return _(o).has("foo");
+});
+test("has returns false when property does not exist", function() {
+  var o = { foo: "bar" };
 
-// (["isElement", "isArray", "isObject", "isFunction", "isBoolean", "isString", "isNumber"]).forEach(function(method) {
-//   test(method + " is defined", function() {
-//     return typeof _[method] === "function" && typeof _()[method] === "function";
-//   });
-// });
-// test("isElement returns true if DOM element, otherwise false", function() {
-//   return _.isElement(document.body) && !_.isElement({});
-// });
-// test("isArray returns true if array, otherwise false", function() {
-//   return _.isArray([]) && !_.isArray({ 0: "a", 1: "b" });
-// });
-// test("isObject returns true if object or function, otherwise false", function() {
-//   return _.isObject({}) && _.isObject([]) && _.isObject(isNaN) && !_.isObject(1);
-// });
-// test("isFunction returns true if function, otherwise false", function() {
-//   return _.isFunction(isNaN) && !_.isFunction({});
-// });
-// test("isBoolean returns true if boolean (primitive or object), otherwise false", function() {
-//   return _.isBoolean(false) && _.isBoolean(new Boolean(false)) && !_.isBoolean(1);
-// });
-// test("isString returns true if string, otherwise false", function() {
-//   return _.isString("") && _.isString(new String()) && !_.isString(1);
-// });
-// test("isNumber returns true if number, (primitive or object), otherwise false", function() {
-//   return _.isNumber(1) && _.isNumber(new Number(5)) && !_.isNumber("5");
-// });
+  return !_(o).has("bar");
+});
+test("has returns true when hasOwnProperty is defined", function() {
+  var o = { foo: "bar" };
+  o.hasOwnProperty = function() { };
+
+  return _(o).has("hasOwnProperty");
+});
+
+(["isElement", "isArray", "isObject", "isFunction", "isBoolean", "isString", "isNumber"]).forEach(function(method) {
+  test(method + " is defined", function() {
+    return typeof _[method] === "function" && typeof _()[method] === "function";
+  });
+});
+
+test("isElement returns true if DOM element, otherwise false", function() {
+  return _.isElement(document.body) && !_.isElement({});
+});
+
+test("object.isElement returns true if DOM element, otherwise false", function() {
+  return _(document.body).isElement() && !_({}).isElement();
+});
+
+test("isArray returns true if array, otherwise false", function() {
+  return _.isArray([]) && !_.isArray({ 0: "a", 1: "b" });
+});
+
+test("object.isArray returns true if array, otherwise false", function() {
+  return _([]).isArray() && !_({ 0: "a", 1: "b" }).isArray();
+});
+
+test("isObject returns true if object or function, otherwise false", function() {
+  return _.isObject({}) && _.isObject([]) && _.isObject(isNaN) && !_.isObject(1);
+});
+
+test("object.isObject returns true if object or function, otherwise false", function() {
+  return _({}).isObject() && _([]).isObject() && _(isNaN).isObject() && !_(1).isObject();
+});
+
+test("isFunction returns true if function, otherwise false", function() {
+  return _.isFunction(isNaN) && !_.isFunction({});
+});
+
+test("object.isFunction returns true if function, otherwise false", function() {
+  return _(isNaN).isFunction() && !_({}).isFunction();
+});
+
+test("isBoolean returns true if boolean (primitive or object), otherwise false", function() {
+  return _.isBoolean(false) && _.isBoolean(new Boolean(false)) && !_.isBoolean(1);
+});
+
+test("object.isBoolean returns true if boolean (primitive or object), otherwise false", function() {
+  return _(false).isBoolean() && _(new Boolean(false)).isBoolean() && !_(1).isBoolean();
+});
+
+test("isString returns true if string, otherwise false", function() {
+  return _.isString("") && _.isString(new String()) && !_.isString(1);
+});
+
+test("object.isString returns true if string, otherwise false", function() {
+  return _("").isString() && _(new String()).isString() && !_(1).isString();
+});
+
+test("isNumber returns true if number, (primitive or object), otherwise false", function() {
+  return _.isNumber(1) && _.isNumber(new Number(5)) && !_.isNumber("5");
+});
+
+test("object.isNumber second way returns true if number, (primitive or object), otherwise false", function() {
+  return _(1).isNumber() && _(new Number(5)).isNumber() && !_("5").isNumber();
+});
